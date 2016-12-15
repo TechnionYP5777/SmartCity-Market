1910e3b8f2e704310ee07bef6a0fe53804d79e24
Noam Yefet <noamyft@gmail.com>
2016-12-15 22:32:09 +0200

    Added static objects (for SQLBuilder) to SQLDatabaseEntities.java. Spartanized classes
    issue: #31

--- Server/src/main/java/SQLDatabase/SQLDatabaseConnection.java
+++ Server/src/main/java/SQLDatabase/SQLDatabaseConnection.java
@@ -1,15 +1,18 @@
 package SQLDatabase;
 
-/** SqlDBConnection - Handles the server request to the SQL database.
+import SQLDatabase.SQLDatabaseEntities;
+import SQLDatabase.SQLDatabaseEntities.IngredientsTable;
+
+/**
+ * SqlDBConnection - Handles the server request to the SQL database.
  * 
  * 
  * @author Noam Yefet
  */
 public class SQLDatabaseConnection {
-	
-	
-	public SQLDatabaseConnection(){
-		
+
+	public SQLDatabaseConnection() {
+
 	}
-	
+
 }
--- Server/src/main/java/SQLDatabase/SQLDatabaseEntities.java
+++ Server/src/main/java/SQLDatabase/SQLDatabaseEntities.java
@@ -1,5 +1,113 @@
 package SQLDatabase;
 
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbColumn;
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbSchema;
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbSpec;
+import com.healthmarketscience.sqlbuilder.dbspec.basic.DbTable;
+
+/**
+ * SQLDatabaseEntities - contains objects to use with sqlbuilder in
+ * {@link SQLDatabaseConnection}
+ * 
+ * @author Noam Yefet
+ */
 class SQLDatabaseEntities {
-	
+
+	static DbSpec spec;
+	static DbSchema databaseSchema;
+
+	static class IngredientsTable {
+		static DbTable ingredientsTable;
+
+		static DbColumn ingredientIDCol;
+		static DbColumn ingredientNameCol;
+	}
+
+	static class ManufacturerTable {
+		static DbTable manufacturerTable;
+
+		static DbColumn manufacturerIDCol;
+		static DbColumn manufacturerNameCol;
+	}
+
+	static class LocationsTable {
+		static DbTable locationsTable;
+
+		static DbColumn locationIDCol;
+		static DbColumn placeInStoreCol;
+		static DbColumn locationDescriptionCol;
+		static DbColumn pointXCol;
+		static DbColumn pointYCol;
+	}
+
+	static class ProductsCatalogTable {
+		static DbTable productsCatalogTable;
+
+		static DbColumn barcodeCol;
+		static DbColumn productNameCol;
+		static DbColumn manufacturerIDCol;
+		static DbColumn productDescriptionCol;
+		static DbColumn productPriceCol;
+		static DbColumn productPictureCol;
+	}
+
+	static class ProductsCatalogIngredientsTable {
+		static DbTable productsCatalogIngredientsTable;
+
+		static DbColumn barcodeCol;
+		static DbColumn ingredientIDCol;
+	}
+
+	static class ProductsCatalogLocationsTable {
+		static DbTable productsCatalogLocationsTable;
+
+		static DbColumn barcodeCol;
+		static DbColumn locationIDCol;
+	}
+
+	static class ProductsPackagesTable {
+		static DbTable productsPackagesTable;
+
+		static DbColumn barcodeCol;
+		static DbColumn locationIDCol;
+		static DbColumn expirationDateCol;
+		static DbColumn amountCol;
+	}
+
+	static class GroceriesListsTable {
+		static DbTable groceriesListsTable;
+
+		static DbColumn listIDCol;
+		static DbColumn barcodeCol;
+		static DbColumn expirationDateCol;
+		static DbColumn amountCol;
+	}
+
+	static class GroceriesListsHistoryTable {
+		static DbTable groceriesListsHistoryTable;
+
+		static DbColumn listIDCol;
+		static DbColumn barcodeCol;
+		static DbColumn expirationDateCol;
+		static DbColumn amountCol;
+	}
+
+	static class CartsListTable {
+		static DbTable cartsListTable;
+
+		static DbColumn cartIDCol;
+		static DbColumn listIDCol;
+	}
+
+	static class WorkersTable {
+		static DbTable workersTable;
+
+		static DbColumn workerIDCol;
+		static DbColumn workerUsernameCol;
+		static DbColumn workerPasswordCol;
+		static DbColumn workerPrivilegesCol;
+		static DbColumn isLoggedInCol;
+		static DbColumn sessionIDCol;
+	}
+
 }
