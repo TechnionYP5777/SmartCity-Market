5bd1a2f2bdef77c7e103860ad5e2e7e492a03f76
Idan Atias <idan2k3@gmail.com>
2016-12-16 13:12:58 +0200

    Changed CommandDescriptor to be more informative & added more result codes <part of issue #72>

--- Common/src/main/java/ClientServerApi/CommandDescriptor.java
+++ Common/src/main/java/ClientServerApi/CommandDescriptor.java
@@ -2,8 +2,13 @@ package ClientServerApi;
 
 public enum CommandDescriptor {
 	/**
-	 * These are the commands we support: When adding a new command, make sure
-	 * it's name is here also.
+	 * ****************************************************************************************************
+	 * These are the commands we support:
+	 *  When adding a new command, make sure you add it's name & description here as well.
+	 * 
+	 * ****************************************************************************************************
+	 * KEEP THE CONVENTIONS!
+	 * ****************************************************************************************************
 	 * 
 	 * @author idan atias
 	 * @author shimon azulay
@@ -11,8 +16,56 @@ public enum CommandDescriptor {
 
 	// Connection:
 	LOGIN,
+	/**
+	 * Description: Client login command to get in the system and receive uniq sender id from server.
+	 * param1: String userName
+	 * param2: String password
+	 * retval: int senderId
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_ALREADY_CONNECTED,
+	 *			SM_USERNAME_DOES_NOT_EXIST,
+	 *			SM_WRONG_PASSWORD,
+	 *			SM_INVALID_PARAMETER,
+	 *
+	 */
+
 	LOGOUT,
+	/**
+	 * Description: Client logout command for logging out of server.
+	 * retval: void
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 * 			SM_INVALID_SENDER_ID,
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *
+	 */
 
 	// Catalog Product:
 	VIEW_PRODUCT_FROM_CATALOG;
+	/**
+	 * Description: Client command for getting the relevant catalog product represented by a barcode.
+	 * param1: int barcode
+	 * retval: CatlogProduct catProduct
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 * 			SM_INVALID_PARAMETER,
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
+	 *
+	 */
 }
--- Common/src/main/java/ClientServerApi/ResultDescriptor.java
+++ Common/src/main/java/ClientServerApi/ResultDescriptor.java
@@ -14,16 +14,20 @@ public enum ResultDescriptor {
 
 // Failure:
 
-	// cmdDescriptor:
+	// cmd:
 	SM_INVALID_CMD_DESCRIPTOR,
 
 	// params:
 	SM_INVALID_PARAMETER,
 
-	// sender:
+	// login/logout:
 	SM_INVALID_SENDER_ID,
 	SM_SENDER_ID_DOES_NOT_EXIST,
 	SM_SENDER_IS_NOT_CONNECTED,
+	SM_SENDER_IS_ALREADY_CONNECTED,
+	SM_USERNAME_DOES_NOT_EXIST,
+	SM_WRONG_PASSWORD,
+	
 
 	// catalog product:
 	SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
