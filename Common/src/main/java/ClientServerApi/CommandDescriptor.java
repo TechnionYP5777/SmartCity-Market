0a716ce0a60f14cbffd721efce5e5777a4fd965c
Aviad Cohen <aviadcohen7@gmail.com>
2016-12-17 09:40:48 +0200

    Common: Added important notes for CommandDescriptor.
    Related issues:
    Added important notes for CommandDescriptor #75
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- Common/src/main/java/ClientServerApi/CommandDescriptor.java
+++ Common/src/main/java/ClientServerApi/CommandDescriptor.java
@@ -9,18 +9,32 @@ public enum CommandDescriptor {
 	 * ****************************************************************************************************
 	 * KEEP THE CONVENTIONS!
 	 * ****************************************************************************************************
+	 * ***** General Notes *****
+	 * 
+	 * 1. retval is relevant only on success (result_code = SM_OK).
+	 * 2. retval will be stored in the data field of the CommandWrapper (unless maintained otherwise).
+	 * 3. The Server/Client will interpret the data field according to the structure which is mentioned in
+	 * the data field, the data will be stored in json.
+	 * 4. When adding new commands, added reference in Github for all project contributes.
+	 * 5. For each command there is option for SM_ERR - a ResultDescriptor for internal failure in the 
+	 * server, such case must be considered always by the client side.
+	 * 
+	 * ****************************************************************************************************
+	 * 
 	 * 
 	 * @author idan atias
 	 * @author shimon azulay
+	 * @author Aviad Cohen
 	 */
 
-	// Connection:
+	/******************************************** Connection **********************************************/
+
 	LOGIN,
 	/**
 	 * Description: Client login command to get in the system and receive unique sender id from server.
 	 * param1: String userName
 	 * param2: String password
-	 * retval: int senderId
+	 * retval: int senderId (in senderId field).
 	 *
 	 * result_codes:
 	 * 		success:
@@ -32,6 +46,8 @@ public enum CommandDescriptor {
 	 *			SM_WRONG_PASSWORD,
 	 *			SM_INVALID_PARAMETER,
 	 *
+	 *	 ***** NOTE *****
+	 * This command has no data from the server side.
 	 */
 
 	LOGOUT,
@@ -48,14 +64,17 @@ public enum CommandDescriptor {
 	 *			SM_SENDER_IS_NOT_CONNECTED,
 	 *			SM_SENDER_ID_DOES_NOT_EXIST,
 	 *
+	 *	 ***** NOTE *****
+	 * This command has no data from the server side.
 	 */
-
-	// Catalog Product:
+	
+	/********************************** Shared employee & cart commands **********************************/
+	
 	VIEW_PRODUCT_FROM_CATALOG;
 	/**
 	 * Description: Client command for getting the relevant catalog product represented by a barcode.
 	 * param1: int barcode
-	 * retval: CatlogProduct catProduct
+	 * retval: CatalogProduct.
 	 *
 	 * result_codes:
 	 * 		success:
