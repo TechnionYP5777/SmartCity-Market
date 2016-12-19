79efc3536f00c5cb3585d71dc5b80427e0c039f7
Aviad Cohen <aviadcohen7@gmail.com>
2016-12-19 15:28:18 +0200

    Server: Integrated between SQL database and CommandHandler
    Related issues:
    Integrate between SQL database and CommandHandler #88
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- Server/src/main/java/CommandHandler/CommandProcess.java
+++ Server/src/main/java/CommandHandler/CommandProcess.java
@@ -15,6 +15,11 @@ import BasicCommonClasses.SmartCode;
 import ClientServerApi.CommandWrapper;
 import ClientServerApi.ResultDescriptor;
 import ClientServerCommunication.ProcessRequest;
+import SQLDatabase.SQLDatabaseConnection;
+import SQLDatabase.SQLDatabaseException.AuthenticationError;
+import SQLDatabase.SQLDatabaseException.CriticalError;
+import SQLDatabase.SQLDatabaseException.ProductNotExistInCatalog;
+import SQLDatabase.SQLDatabaseException.WorkerNotConnected;
 
 public class CommandProcess implements ProcessRequest {
 
@@ -25,36 +30,84 @@ public class CommandProcess implements ProcessRequest {
 	
 	private void loginCommand() {		
 		Login login;
+		SQLDatabaseConnection sqlDatabaseConnection = new SQLDatabaseConnection();
 		
 		LOGGER.log(Level.FINE, "Login command called");
 		
 		login = new Gson().fromJson(inCommandWrapper.getData(), Login.class);
-		
-		//TODO Noam - call SQL Login here and insert result to outCommandWrapper using try-catch
+				
+		try {
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_OK);
+			outCommandWrapper.setSenderID((sqlDatabaseConnection.WorkerLogin(login.getUserName(), login.getPassword())));
+			
+			LOGGER.log(Level.FINE, "Login command succeded with sender ID " + outCommandWrapper.getSenderID());
+		} catch (AuthenticationError e) {
+			LOGGER.log(Level.FINE, "Login command failed, username dosen't exist or wrong password received");
+
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD);			
+		} catch (CriticalError e) {
+			LOGGER.log(Level.SEVERE, "Login command failed, critical error occured from SQL Database connection");
+			
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_ERR);
+			e.printStackTrace();
+		}
 		
 		LOGGER.log(Level.FINE, "Login with User " + login.getUserName() + " finished");
 	}
 	
 	private void logoutCommand() {
 		String username;
+		SQLDatabaseConnection sqlDatabaseConnection = new SQLDatabaseConnection();
 		
 		LOGGER.log(Level.FINE, "Logout command called");
 		
 		username = new Gson().fromJson(inCommandWrapper.getData(), String.class);
 		
-		//TODO Noam - call SQL Logout here and insert result to outCommandWrapper using try-catch
+		try {
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_OK);
+			sqlDatabaseConnection.WorkerLogout(inCommandWrapper.getSenderID(), username);
+			
+			LOGGER.log(Level.FINE, "Logout command succeded with sender ID " + inCommandWrapper.getSenderID());
+		} catch (WorkerNotConnected e) {
+			LOGGER.log(Level.FINE, "Logout command failed, username dosen't login to the system");
+
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_SENDER_IS_NOT_CONNECTED);			
+		} catch (CriticalError e) {
+			LOGGER.log(Level.SEVERE, "Logout command failed, critical error occured from SQL Database connection");
+
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_ERR);
+			e.printStackTrace();
+		}
 		
 		LOGGER.log(Level.FINE, "Logout with User " + username + " finished");
 	}
 	
 	private void viewProductFromCatalogCommand() {
 		SmartCode smartCode;
+		SQLDatabaseConnection sqlDatabaseConnection = new SQLDatabaseConnection();
 		
 		LOGGER.log(Level.FINE, "View Product From Catalog command called");
 		
 		smartCode = new Gson().fromJson(inCommandWrapper.getData(), SmartCode.class);
 		
-		//TODO Noam - call SQL View Product From Catalog here and insert result to outCommandWrapper using try-catch
+		try {
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_OK, sqlDatabaseConnection.getProductFromCatalog(inCommandWrapper.getSenderID(), smartCode.getBarcode()));
+			
+			LOGGER.log(Level.FINE, "Get product from catalog command secceeded with barcode " + smartCode.getBarcode());
+		} catch (ProductNotExistInCatalog e) {
+			LOGGER.log(Level.FINE, "Get product from catalog command failed, product dosen't exist in the system");
+
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_CATALOG_PRODUCT_DOES_NOT_EXIST);
+		} catch (WorkerNotConnected e) {
+			LOGGER.log(Level.FINE, "Get product from catalog command failed, username dosen't login to the system");
+
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_SENDER_IS_NOT_CONNECTED);
+		} catch (CriticalError e) {
+			LOGGER.log(Level.SEVERE, "Get product from catalog command failed, critical error occured from SQL Database connection");
+
+			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_ERR);
+			e.printStackTrace();
+		}
 		
 		LOGGER.log(Level.FINE, "View Product From Catalog with product barcode  " + smartCode.getBarcode() + " finished");
 	}
