52fb363ad7261db8f73a8387a3be6c85e163d941
Aviad Cohen <aviadcohen7@gmail.com>
2016-12-25 00:02:32 +0200

    Common: Added all relevant Commands to the commands API for sprint 2
    Relevant issues:
    
    Add all relevant Commands to the commands API for sprint 2 #102
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- Common/src/main/java/ClientServerApi/CommandDescriptor.java
+++ Common/src/main/java/ClientServerApi/CommandDescriptor.java
@@ -3,9 +3,9 @@ package ClientServerApi;
 public enum CommandDescriptor {
 	/**
 	 * ****************************************************************************************************
-	 * These are the commands we support:
+	 *
 	 *  When adding a new command, make sure you add it's name & description here as well.
-	 * 
+	 *
 	 * ****************************************************************************************************
 	 * KEEP THE CONVENTIONS!
 	 * ****************************************************************************************************
@@ -14,10 +14,14 @@ public enum CommandDescriptor {
 	 * 1. retval is relevant only on success (result_code = SM_OK).
 	 * 2. retval will be stored in the data field of the CommandWrapper (unless maintained otherwise).
 	 * 3. The Server/Client will interpret the data field according to the structure which is mentioned in
-	 * the data field, the data will be stored in json.
+	 *	  the data field, the data will be stored in json.
 	 * 4. When adding new commands, added reference in Github for all project contributes.
-	 * 5. For each command there is option for SM_ERR - a ResultDescriptor for internal failure in the 
-	 * server, such case must be considered always by the client side.
+	 * 5. *** IMPORTANT ***
+	 * 
+	 *    For each command there is also option to get one of the following ResultDescriptors:
+	 *    a. SM_ERR - a ResultDescriptor for internal failure in the 
+	 * 	  server, such case must be considered always by the client side.
+	 * 	  b. SM_INVALID_PARAMETER - one of the given parameter has invalid format.
 	 * 
 	 * ****************************************************************************************************
 	 * 
@@ -70,10 +74,10 @@ public enum CommandDescriptor {
 	
 	/********************************** Shared employee & cart commands **********************************/
 	
-	VIEW_PRODUCT_FROM_CATALOG;
+	VIEW_PRODUCT_FROM_CATALOG,
 	/**
 	 * Description: Client command for getting the relevant catalog product represented by a barcode.
-	 * param1: SmartCode - smartcode with barcode and null on expertionDate
+	 * param1: SmartCode - Smartcode with barcode and null on expertionDate.
 	 * retval: CatalogProduct.
 	 *
 	 * result_codes:
@@ -81,10 +85,169 @@ public enum CommandDescriptor {
 	 * 			SM_OK,
 	 * 		
 	 * 		failure:
-	 * 			SM_INVALID_PARAMETER,
 	 *			SM_SENDER_IS_NOT_CONNECTED,
 	 *			SM_SENDER_ID_DOES_NOT_EXIST,
 	 *			SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
+	 *
+	 */
+	
+	ADD_PRODUCT_PACKAGE_TO_WAREHOUSE,
+	/**
+	 * Description: Employee add new ProductPackage to warehouse.
+	 * param1: ProductPackage - The ProductPackage which will be add to warehouse.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
+	 *
+	 */
+	
+	ADD_PRODUCT_TO_CATALOG,
+	/**
+	 * Description: Manager add new CatalogProduct to the market catalog.
+	 * param1: CatalogProduct - The CatalogProduct which will be add to the market catalog.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_CATALOG_PRODUCT_ALREADY_EXISTS,
+	 *
+	 */
+	
+	REMOVE_PRODUCT_FROM_CATALOG,
+	/**
+	 * Description: Manager remove CatalogProduct from the market catalog.
+	 * param1: SmartCode - Smartcode with barcode and null on expertionDate.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
+	 *			SM_CATALOG_PRODUCT_STILL_FOR_SALE,
+	 *
+	 */
+	
+	ADD_PRODUCT_TO_GROCERY_LIST,
+	/**
+	 * Description: Cart add product to grocery list.
+	 * param1: ProductPackage - ProductPackage with SmartCode, amount and null on location.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_NOT_ENOUGH_AMOUNT,
+	 *
+	 */
+	
+	REMOVE_PRODUCT_FROM_GROCERY_LIST,
+	/**
+	 * Description: Cart remove product from grocery list.
+	 * param1: ProductPackage - ProductPackage with SmartCode, amount and null on location.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_AMOUNT_BIGGER_THEN_AVAILABLE,
+	 *
+	 */
+	
+	EDIT_PRODUCT_FROM_CATALOG,
+	/**
+	 * Description: Manager edit product from the catalog.
+	 * param1: SmartCode - Smartcode with barcode and null on expertionDate.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
+	 *
+	 */
+
+	PLACE_PRODUCT_PACKAGE_ON_SHELVES,
+	/**
+	 * Description: Employee move product package from warehouse to shelves.
+	 * param1: ProductPackage - ProductPackage with SmartCode of the ProductPackage from the warehouse,
+	 *                          amount to move and the new location of the ProductPackage.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_AMOUNT_BIGGER_THEN_AVAILABLE,
+	 *
+	 */
+	
+	REMOVE_PRODUCT_PACKAGE_FROM_STORE,
+	/**
+	 * Description: Employee remove product package from warehouse or shelves.
+	 * param1: ProductPackage - ProductPackage with SmartCode of the ProductPackage,
+	 *                          amount to remove and the location of the ProductPackage.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_DOES_NOT_EXIST,
+	 *			SM_PRODUCT_PACKAGE_AMOUNT_BIGGER_THEN_AVAILABLE,
+	 *
+	 */
+	
+	CHECKOUT_GROCERY_LIST,
+	/**
+	 * Description: Cart checkout it's current active grocery list.
+	 * param1: void.
+	 * retval: void.
+	 *
+	 * result_codes:
+	 * 		success:
+	 * 			SM_OK,
+	 * 		
+	 * 		failure:
+	 *			SM_SENDER_IS_NOT_CONNECTED,
+	 *			SM_SENDER_ID_DOES_NOT_EXIST,
+	 *			SM_GROCERY_LIST_IS_EMPTY,
 	 *
 	 */
 }
--- Common/src/main/java/ClientServerApi/ResultDescriptor.java
+++ Common/src/main/java/ClientServerApi/ResultDescriptor.java
@@ -12,78 +12,75 @@ public enum ResultDescriptor {
 
 /************************************************ Success ***************************************************/
 	
+	SM_OK,
 	/**
-	 * SM_OK
-	 ********************************************************************************************************
-	 *
 	 * The command request accepted by the server and was executed successfully.
 	 */
-	SM_OK,
 
 /************************************************ Failures **************************************************/
 
 	/**************************************** General Failures **********************************************/
 	
-	/**
-	 * SM_INVALID_CMD_DESCRIPTOR
-	 ********************************************************************************************************
-	 *
-	 * The command request denied due to unsupported command by the server.
-	 */
 	SM_INVALID_CMD_DESCRIPTOR,
-
 	/**
-	 * SM_INVALID_PARAMETER
-	 ********************************************************************************************************
-	 *
-	 * The command request has invalid parameter.
+	 * The command request denied due to unsupported command by the server.
+	 * 
+	 * How to fix:
+	 * 
+	 * 	1. Report critical error (Check for bug when see such message).
+	 * 	2. Try to send the command again (maybe the command failed due to network errors).
+	 * 
 	 */
-	SM_INVALID_PARAMETER,
 
+	SM_INVALID_PARAMETER,
 	/**
-	 * SM_INVALID_PARAMETER
-	 ********************************************************************************************************
-	 *
 	 * The command request has invalid parameter.
+	 * The format of one of the parameters is illegal.
+	 * i.e.: The client inserted negative in senderID in command which must have legal senderID.
+	 * 
+	 * How to fix:
+	 * 
+	 * 	1. Report critical error (Check for a bug when see such message) since such message
+	 *     isn't supposed to be sent to the server.
+	 * 
 	 */
 	
+	SM_ERR,
 	/**
-	 * SM_ERR
-	 ********************************************************************************************************
-	 *
-	 * The command request failed to general error (might be due to server initial failure).
+	 * The command request failed due to general error (might be due to server initial failure).
+	 * 
+	 * How to fix:
+	 * 
+	 * 1. Report critical error (Check for a bug when see such message).
+	 * 2. Try to send the message again (maybe the command failed due to network errors).
 	 *
 	 */
-	SM_ERR,
 
 	/****************************************** Login/Logout failures **************************************/
 
+	SM_INVALID_SENDER_ID,
 	/**
-	 * SM_INVALID_SENDER_ID
-	 ********************************************************************************************************
-	 *
 	 * The command request has invalid sender id (Does not exist in the server database).
 	 * 
 	 * How to fix:
-	 * 1. Send Login command.
-	 */
-	SM_INVALID_SENDER_ID,
-	
-	/**
-	 * SM_SENDER_IS_NOT_CONNECTED
-	 ********************************************************************************************************
+	 * 
+	 * 1. Report critical error (Check for a bug when see such message).
 	 *
+	 */
+	
+	SM_SENDER_IS_NOT_CONNECTED,
+	/**
 	 * The sender is not connected to the system but tried to execute command still.
 	 * 
 	 * How to fix:
-	 * 1. Send Login command.
+	 * 
+	 * 1. Report critical error (Check for a bug when see such message).
+	 * 2. Ask the user to login again.
+	 * 
 	 */
-	SM_SENDER_IS_NOT_CONNECTED,
 	
+	SM_SENDER_IS_ALREADY_CONNECTED,
 	/**
-	 * SM_SENDER_IS_ALREADY_CONNECTED
-	 ********************************************************************************************************
-	 *
 	 * The sender tried to login to the server although he is already connected.
 	 * 
 	 * This might be caused due to:
@@ -92,36 +89,98 @@ public enum ResultDescriptor {
 	 * 
 	 * How to fix:
 	 *
-	 * 1. Send Logout command using the same sender id.
-	 * 2. Send Login command.
+	 * 1. Report critical error (Check for a bug when see such message).
+	 * 2. Ask the user to logout and login again.
+	 * 
 	 */
-	SM_SENDER_IS_ALREADY_CONNECTED,
 	
+	SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD,
 	/**
-	 * SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD
-	 ********************************************************************************************************
-	 *
 	 * The sender tried to connect to the server with unknown username or with wrong password.
 	 * 
 	 * Due to security reasons the user shouldn't know if the username exists in the server database
 	 * or he inserted a wrong password.
+	 * 
+	 * How to fix:
+	 * 
+	 * 1. Ask the user to login again with another username/password.
+	 * 
 	 */
-	SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD,
 	
 	/********************************** Shared failures for employee & cart ********************************/
 	
+	SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
 	/**
-	 * SM_CATALOG_PRODUCT_DOES_NOT_EXIST
-	 ********************************************************************************************************
-	 *
 	 * The sender tried to get invalid product.
 	 * 
 	 * How to fix:
-	 * 1. IF the smartcode was a valid smartcode - please add to the server database, otherwise - ignore.
+	 * 
+	 * 1. If the Smartcode was a valid Smartcode - please add to the server database.
+	 * 2. Otherwise - report critical error (Check for a bug when see such message).
+	 * 
 	 */
-	SM_CATALOG_PRODUCT_DOES_NOT_EXIST,
 	
 	/************************************** Employee failures only *****************************************/
+	
+	SM_CATALOG_PRODUCT_ALREADY_EXISTS,
+	/**
+	 * The sender tried to add catalog product with existing barcode.
+	 * 
+	 * How to fix:
+	 * 1. Report critical error (Check for a bug when see such message).
+	 * 2. Tell the employee that the given barcode already exists in the market catalog and he may choose:
+	 * 		a. Change the given barcode.
+	 * 		b. Edit the current product using the new data.
+	 * 		c. Fetch information from the database regarding the same barcode.
+	 * 		d. Abort operation.
+	 * 
+	 */
+	
+	SM_CATALOG_PRODUCT_STILL_FOR_SALE,
+	/**
+	 * The manager tried to remove catalog product which still available for sale.
+	 * 
+	 * How to fix:
+	 * 1. Tell the manager that the given product still available for sale and he may choose:
+	 * 		a. Change the given barcode.
+	 * 		b. Remove the CatalogProduct anyway and remove all existing packages of the product.
+	 * 		c. Abort operation.
+	 * 
+	 */
+	
+	SM_PRODUCT_PACKAGE_AMOUNT_BIGGER_THEN_AVAILABLE,
+	/**
+	 * The amount from the selected package is not available.
+	 * 
+	 * How to fix:
+	 * 1. Tell the cart/employee that the given amount is illegal and he may choose:
+	 * 		a. Change the given amount.
+	 * 		b. Abort operation.
+	 * 
+	 */
 
 	/***************************************** Cart failures only ******************************************/
+	
+	SM_PRODUCT_PACKAGE_DOES_NOT_EXIST,
+	/**
+	 * The cart tried to add product package to grocery list which dosen't exist.
+	 * 
+	 * How to fix:
+	 * 1. Report critical error (Check for a bug when see such message).
+	 * 2. Tell the cart that the given product not available for sale (or not in the grocery list) and he may choose:
+	 * 		a. Change the given SmartCode.
+	 * 		b. Abort operation.
+	 * 
+	 */
+	
+	SM_GROCERY_LIST_IS_EMPTY,
+	/**
+	 * The cart tried to checkout an empty grocery list.
+	 * 
+	 * How to fix:
+	 * 1. Report critical error (Check for a bug when see such message) - checkout command
+	 *    isn't supposed to be sent when grocery list is empty.
+	 * 2. Tell the cart that the given grocery list is empty and can't be checked-out.
+	 * 
+	 */
 }
