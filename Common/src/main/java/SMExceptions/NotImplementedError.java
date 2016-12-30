71cb4e3d185a2a684acd2d5f02f6ef6dd9b26539
Idan Atias <idan2k3@gmail.com>
2016-12-30 03:23:25 +0200

    Added a "NotImplementedError" exception (part of #135)
    Use this exception for forcing a derived class to implement a specific base class method
    
    Signed-off-by: Idan Atias <idan2k3@gmail.com>

---
+++ Common/src/main/java/SMExceptions/NotImplementedError.java
@@ -0,0 +1,16 @@
+package SMExceptions;
+
+/**
+ * Use this exception for forcing a derived class to implement a specific base class method
+ * @author idan
+ *
+ */
+
+public class NotImplementedError extends SMException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4265323783375415676L;
+
+}
