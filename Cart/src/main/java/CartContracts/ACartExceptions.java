58282329cac5aa37ed08395b64518a3c96b19474
LiorBenAmi <benamilior89@gmail.com>
2017-01-04 22:36:42 +0200

    change cart exceptions serialVersionUID #186

--- Cart/src/main/java/CartContracts/ACartExceptions.java
+++ Cart/src/main/java/CartContracts/ACartExceptions.java
@@ -10,54 +10,48 @@ import SMExceptions.SMException;
  */
 public class ACartExceptions extends SMException {
 
-	private static final long serialVersionUID = 8663454971017928972L;
+	private static final long serialVersionUID = -7538312012360295744L;
 
 	/**
 	 * Thrown when invalid (unsupported) command was sent by worker.
 	 */
 	public static class InvalidCommandDescriptor extends ACartExceptions {
-
-		private static final long serialVersionUID = -6013813473192780331L;
+		private static final long serialVersionUID = 5925504076668539623L;
 	}
 	
 	/**
 	 * Thrown when one of the parameters is illegal.
 	 */
 	public static class InvalidParameter extends ACartExceptions {
-
-		private static final long serialVersionUID = 6423454812287539804L;
-
+		private static final long serialVersionUID = -7605966916151865742L;
 	}
 	
 	/**
 	 * Thrown when unexpected error occurred
 	 */
 	public static class CriticalError extends ACartExceptions {
-
-		private static final long serialVersionUID = -1281316098307233162L;
+		private static final long serialVersionUID = -1013582717042687901L;
 	}
 	
 	/**
 	 * Thrown when worker try to do operation before connecting
 	 */
 	public static class CartNotConnected extends ACartExceptions {
-		private static final long serialVersionUID = 736110846693773042L;	
+		private static final long serialVersionUID = 4708231565561825649L;
 	}
 
 	/**
 	 * Thrown when worker try to connect twice
 	 */
 	public static class ProductNotInCart extends ACartExceptions {
-
-		private static final long serialVersionUID = -4880371025977064054L;
+		private static final long serialVersionUID = -2078563581704067033L;
 	}
 	
 	/**
 	 * Thrown when worker not exist in database or password don't match
 	 */
 	public static class AuthenticationError extends ACartExceptions {
-
-		private static final long serialVersionUID = -3035025128800993047L;
+		private static final long serialVersionUID = -1202631123044106836L;
 	}
 
 	/**
@@ -65,26 +59,20 @@ public class ACartExceptions extends SMException {
 	 */
 	public static class AmountBiggerThanAvailable extends ACartExceptions {
 
-		private static final long serialVersionUID = -6977996976396033467L;
+		private static final long serialVersionUID = 1912331911777395334L;
 	}
 	
 	/**
 	 * Thrown when product package does not exist.
 	 */
 	public static class ProductPackageDoesNotExist extends ACartExceptions {
-
-		private static final long serialVersionUID = -1086760036316621806L;
+		private static final long serialVersionUID = -6889959552098680402L;
 	}
 	
 	/**
 	 * Thrown when try to checkout empty grocery list.
 	 */
 	public static class GroceryListIsEmpty extends ACartExceptions {
-
-		private static final long serialVersionUID = 231336810775816368L;
+		private static final long serialVersionUID = 977822169247328736L;
 	}
-	
-	/**
-	 * Thrown when requesting product already exist in catalog
-	 */
 }
