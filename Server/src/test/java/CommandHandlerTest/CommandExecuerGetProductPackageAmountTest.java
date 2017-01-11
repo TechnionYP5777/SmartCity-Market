563cdf2909771bf56374076df6d644de19fffdf6
Aviad Cohen <aviadcohen7@gmail.com>
2017-01-11 22:13:56 +0200

    Server: Added unit test to GET_PRODUCT_PACKAGE_AMOUNT command in server
    Related issues:
    
    Add unit test to GET_PRODUCT_PACKAGE_AMOUNT command in server #217
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

---
+++ Server/src/test/java/CommandHandlerTest/CommandExecuerGetProductPackageAmountTest.java
@@ -0,0 +1,135 @@
+package CommandHandlerTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.gson.Gson;
+
+import BasicCommonClasses.Location;
+import BasicCommonClasses.PlaceInMarket;
+import BasicCommonClasses.ProductPackage;
+import BasicCommonClasses.SmartCode;
+import ClientServerApi.CommandDescriptor;
+import ClientServerApi.CommandWrapper;
+import ClientServerApi.ResultDescriptor;
+import CommandHandler.CommandExecuter;
+import SQLDatabase.SQLDatabaseConnection;
+import SQLDatabase.SQLDatabaseException.ClientNotConnected;
+import SQLDatabase.SQLDatabaseException.CriticalError;
+import SQLDatabase.SQLDatabaseException.ProductNotExistInCatalog;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CommandExecuerGetProductPackageAmountTest {
+
+	@Mock
+	private SQLDatabaseConnection sqlDatabaseConnection;
+
+	@Before
+	public void setup() {
+		PropertyConfigurator.configure("../log4j.properties");
+	}
+	
+	@Test
+	public void getProductPackageAmountSuccessfulTest() {
+		int senderID = 1;
+		ProductPackage productPackage = new ProductPackage(new SmartCode(1, null), 1, new Location(0, 0, PlaceInMarket.WAREHOUSE));
+		String command = new CommandWrapper(senderID, CommandDescriptor.GET_PRODUCT_PACKAGE_AMOUNT,
+				new Gson().toJson(productPackage, ProductPackage.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			(Mockito.when(!productPackage.getLocation().equals(PlaceInMarket.WAREHOUSE)
+					? sqlDatabaseConnection.getProductPackageAmonutOnShelves(senderID, productPackage)
+					: sqlDatabaseConnection.getProductPackageAmonutInWarehouse(senderID, productPackage)))
+							.thenReturn("");
+		} catch (CriticalError | ClientNotConnected | ProductNotExistInCatalog e1) {
+			e1.printStackTrace();
+			fail();
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_OK, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void getProductPackageAmountCriticalErrorTest() {
+		int senderID = 1;
+		ProductPackage productPackage = new ProductPackage(new SmartCode(1, null), 1, new Location(0, 0, PlaceInMarket.WAREHOUSE));
+		String command = new CommandWrapper(senderID, CommandDescriptor.GET_PRODUCT_PACKAGE_AMOUNT,
+				new Gson().toJson(productPackage, ProductPackage.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.when(sqlDatabaseConnection.getProductPackageAmonutOnShelves(senderID, productPackage)).thenThrow(new CriticalError());
+			Mockito.when(sqlDatabaseConnection.getProductPackageAmonutInWarehouse(senderID, productPackage)).thenThrow(new CriticalError());
+		} catch (ClientNotConnected | ProductNotExistInCatalog e1) {
+			e1.printStackTrace();
+			fail();
+		} catch (CriticalError __) {
+			/* Success */
+		}
+				
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_ERR, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void getProductPackageAmountClientNotConnectedTest() {
+		int senderID = 1;
+		ProductPackage productPackage = new ProductPackage(new SmartCode(1, null), 1, new Location(0, 0, PlaceInMarket.WAREHOUSE));
+		String command = new CommandWrapper(senderID, CommandDescriptor.GET_PRODUCT_PACKAGE_AMOUNT,
+				new Gson().toJson(productPackage, ProductPackage.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.when(sqlDatabaseConnection.getProductPackageAmonutOnShelves(senderID, productPackage)).thenThrow(new ClientNotConnected());
+			Mockito.when(sqlDatabaseConnection.getProductPackageAmonutInWarehouse(senderID, productPackage)).thenThrow(new ClientNotConnected());
+		} catch (CriticalError | ProductNotExistInCatalog e1) {
+			e1.printStackTrace();
+			fail();
+		} catch (ClientNotConnected __) {
+			/* Success */
+		}
+				
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_SENDER_IS_NOT_CONNECTED, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void getProductPackageAmountProductNotExistInCatalogTest() {
+		int senderID = 1;
+		ProductPackage productPackage = new ProductPackage(new SmartCode(1, null), 1, new Location(0, 0, PlaceInMarket.WAREHOUSE));
+		String command = new CommandWrapper(senderID, CommandDescriptor.GET_PRODUCT_PACKAGE_AMOUNT,
+				new Gson().toJson(productPackage, ProductPackage.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.when(sqlDatabaseConnection.getProductPackageAmonutOnShelves(senderID, productPackage)).thenThrow(new ProductNotExistInCatalog());
+			Mockito.when(sqlDatabaseConnection.getProductPackageAmonutInWarehouse(senderID, productPackage)).thenThrow(new ProductNotExistInCatalog());
+		} catch (CriticalError | ClientNotConnected e1) {
+			e1.printStackTrace();
+			fail();
+		} catch (ProductNotExistInCatalog __) {
+			/* Success */
+		}
+				
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_CATALOG_PRODUCT_DOES_NOT_EXIST, out.getResultDescriptor());
+	}
+}
