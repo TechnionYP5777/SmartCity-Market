07ca491aef4232321ef6b6db9275cffc807bc301
Aviad Cohen <aviadcohen7@gmail.com>
2017-01-15 23:17:35 +0200

    Server: Added unittest to IS_LOGGED_IN command on server
    Related issues:
    
    Add unittest to IS_LOGGED_IN command on server #229
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

---
+++ Server/src/test/java/CommandHandlerTest/CommandExecuterIsLoggedInTest.java
@@ -0,0 +1,75 @@
+package CommandHandlerTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.gson.Gson;
+
+import ClientServerApi.CommandDescriptor;
+import ClientServerApi.CommandWrapper;
+import ClientServerApi.ResultDescriptor;
+import CommandHandler.CommandExecuter;
+import SQLDatabase.SQLDatabaseConnection;
+import SQLDatabase.SQLDatabaseException.CriticalError;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CommandExecuterIsLoggedInTest {
+		
+	@Mock
+	private SQLDatabaseConnection sqlDatabaseConnection;
+
+	@Before
+	public void setup() {
+		PropertyConfigurator.configure("../log4j.properties");
+	}
+	
+	@Test
+	public void isLoggedInSuccessfulTest() {
+		int senderID = 0;
+		String command = new CommandWrapper(senderID, CommandDescriptor.IS_LOGGED_IN).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.when(
+					sqlDatabaseConnection.isClientLoggedIn(senderID)).thenReturn(true);
+		} catch (CriticalError e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(true, new Gson().fromJson(out.getData(), Boolean.class));
+		assertEquals(ResultDescriptor.SM_OK, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void isLoggedInCriticalErrorTest() {
+		int senderID = 0;
+		String command = new CommandWrapper(senderID, CommandDescriptor.IS_LOGGED_IN).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			
+			Mockito.when(
+					sqlDatabaseConnection.isClientLoggedIn(senderID)).thenThrow(new CriticalError());
+		} catch (CriticalError e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_ERR, out.getResultDescriptor());
+	}
+}
