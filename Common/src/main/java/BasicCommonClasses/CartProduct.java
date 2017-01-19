104b5d1f49743ff543a3517d9896ea6fcc2e6a39
LiorBenAmi <benamilior89@gmail.com>
2017-01-19 20:34:03 +0200

    revert last

--- Common/src/main/java/BasicCommonClasses/CartProduct.java
+++ Common/src/main/java/BasicCommonClasses/CartProduct.java
@@ -1,7 +1,6 @@
 package BasicCommonClasses;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 
 /** CartProduct - The product from the Cart (/costumer) perspective. 
  * @param amount - The number of items of the same product in the cart.
@@ -10,14 +9,14 @@ import java.util.HashSet;
  * @since 2016-12-09 */
 public class CartProduct {
 	CatalogProduct catalogProduct;
-	HashSet<ProductPackage> packages;
-	int totalAmount;
+	LocalDate expirationDate;
+	int amount;
 	
-	public CartProduct(CatalogProduct catalogProduct, HashSet<ProductPackage> packages,
-			int totalAmount) {
+	public CartProduct(CatalogProduct catalogProduct, LocalDate expirationDate, 
+			int amount) {
 		this.catalogProduct = catalogProduct;
-		this.packages =packages;
-		this.totalAmount = totalAmount;
+		this.expirationDate = expirationDate;
+		this.amount = amount;
 	}
 
 	public CatalogProduct getCatalogProduct() {
@@ -28,19 +27,52 @@ public class CartProduct {
 		this.catalogProduct = ¢;
 	}
 
-	public HashSet<ProductPackage> getPackages() {
-		return packages;
+	public LocalDate getExpirationDate() {
+		return expirationDate;
 	}
 
-	public void setPackages(HashSet<ProductPackage> ¢) {
-		this.packages = ¢;
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate = expirationDate;
 	}
 
-	public int getTotalAmount() {
-		return totalAmount;
+	public int getAmount() {
+		return amount;
 	}
 
-	public void setTotalAmount(int totalAmount) {
-		this.totalAmount = totalAmount;
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public void incrementAmount(int ¢) {
+		amount += ¢;
+	}
+	
+	public void decreaseAmount(int ¢) {
+		amount = amount - ¢ <= 0 ? 0 : amount - ¢;
+	}
+	@Override
+	public int hashCode() {
+		return 31 * (((catalogProduct == null) ? 0 : catalogProduct.hashCode()) + 31)
+				+ ((expirationDate == null) ? 0 : expirationDate.hashCode());
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		CartProduct other = (CartProduct) o;
+		if (catalogProduct == null) {
+			if (other.catalogProduct != null)
+				return false;
+		} else if (!catalogProduct.equals(other.catalogProduct))
+			return false;
+		if (expirationDate == null) {
+			if (other.expirationDate != null)
+				return false;
+		} else if (!expirationDate.equals(other.expirationDate))
+			return false;
+		return true;
 	}
 }
