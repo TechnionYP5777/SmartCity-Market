8789cb5c34760f7cd4d79a69b9edc7f77c2d94d0
Shimon Azulay <sazulay3@gmail.com>
2017-01-22 08:57:39 -0800

    Create CSS for Employee menu #257
    add logout button

--- Employee/src/main/java/EmployeeGui/EmployeeMenuScreen.java
+++ Employee/src/main/java/EmployeeGui/EmployeeMenuScreen.java
@@ -17,6 +17,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
@@ -46,6 +47,9 @@ public class EmployeeMenuScreen implements Initializable {
 
 	@FXML
 	Tab manageCatalogProductTab;
+	
+	@FXML
+	Button logout;
 
 	IWorker worker;
 
