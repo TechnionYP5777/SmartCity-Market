40855c27dcf4c63f8a0eeb2d9d88f8c1f5391cf6
LiorBenAmi <benamilior89@gmail.com>
2017-01-22 02:28:11 +0200

    add exception handler #260

---
+++ Cart/src/main/java/CartGui/CartGuiExceptionsHandler.java
@@ -0,0 +1,45 @@
+package CartGui;
+
+import CartContracts.ACartExceptions.*;
+import CommonDefs.GuiCommonDefs;
+import GuiUtils.DialogMessagesService;
+import GuiUtils.GuiExceptionHandler;
+import SMExceptions.SMException;
+
+public class CartGuiExceptionsHandler extends GuiExceptionHandler {
+	public static void handle(SMException ¢) {
+		if (¢ instanceof InvalidCommandDescriptor)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.criticalErrorTitle, null,
+					GuiCommonDefs.criticalErrorMsg);
+		else if (¢ instanceof InvalidParameter)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.criticalErrorTitle, null,
+					GuiCommonDefs.criticalErrorMsg);
+		else if (¢ instanceof CriticalError)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.criticalErrorTitle, null,
+					GuiCommonDefs.criticalErrorMsg);
+		else if (¢ instanceof AuthenticationError)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.loginFailureDialogTitle, null,
+					GuiCommonDefs.criticalErrorMsg);
+		else if (¢ instanceof CartNotConnected)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.loginFailureDialogTitle, null,
+					GuiCommonDefs.criticalErrorMsg);
+		else if (¢ instanceof ProductNotInCart)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.productOperationFailureTitle, null,
+					GuiCommonDefs.productNotInCartMsg);
+		else if (¢ instanceof AmountBiggerThanAvailable)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.notEnoughAmountInStore, null,
+					GuiCommonDefs.productCapacityIsNotEnoughMsg);
+		else if (¢ instanceof ProductPackageDoesNotExist)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.productOperationFailureTitle, null,
+					GuiCommonDefs.productNotExistsInCatalogMsg);
+		else if (¢ instanceof GroceryListIsEmpty)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.purchaseOperationFailureTitle, null,
+					GuiCommonDefs.groceryListIseEmptyMsg);
+		else if (¢ instanceof ProductCatalogDoesNotExist)
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.productOperationFailureTitle, null,
+					GuiCommonDefs.productNotExistsInCatalogMsg);
+		else
+			DialogMessagesService.showErrorDialog(GuiCommonDefs.productOperationFailureTitle, null,
+					GuiCommonDefs.productPackageDoesNotExistMsg);
+	}
+}
