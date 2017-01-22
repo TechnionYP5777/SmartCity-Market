979c416dbbbdedc4057f9dbb8d79b20a19e73c0a
Idan Atias <idan2k3@gmail.com>
2017-01-22 20:13:51 +0200

    added list view cell orientation #254
    Signed-off-by: Idan Atias <idan2k3@gmail.com>

--- Cart/src/main/java/CartGui/CartProductCellFormat.java
+++ Cart/src/main/java/CartGui/CartProductCellFormat.java
@@ -1,23 +1,15 @@
 package CartGui;
 
 import BasicCommonClasses.CartProduct;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+//import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class CartProductCellFormat extends ListCell<CartProduct> {
-//
-//	private VBox cellContainer;
-//
-//	private Label productName;
-//
-//	private Label productAmount;
-//
-//	private Label productPrice;
-//
-//	private ImageView productImage;
 
 	@Override
 	public void updateItem(CartProduct item, boolean empty) {
@@ -28,14 +20,15 @@ public class CartProductCellFormat extends ListCell<CartProduct> {
 			setText(null);
 			return;
 		}
-		
+
 		VBox vbx = new VBox(5); // spacing = 5
-		Label productName = new Label(item.getCatalogProduct().getName());
-		Label productAmount = new Label("1");
-		Label productPrice = new Label(new Double(item.getCatalogProduct().getPrice()).toString());
+		Label productName = new Label("Name: " + item.getCatalogProduct().getName());
+		Label productAmount = new Label("Amount: " + "1");
+		Label productPrice = new Label("Price: " + new Double(item.getCatalogProduct().getPrice()).toString());
 		Image image = new Image("http://www.toprechesh.co.il/files/products/image1_2102_2016-02-14_13-46-36.jpg");
 		ImageView productImage = new ImageView(image);
 	    vbx.getChildren().addAll(productName, productAmount, productPrice, productImage);
+	    vbx.setAlignment(Pos.CENTER);
 
 		setGraphic(vbx);
 	}
