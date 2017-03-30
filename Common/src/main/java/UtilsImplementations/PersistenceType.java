f99ddf2b8cb278aecec12246c260f61f66d37af3
Shimon Azulay <sazulay3@gmail.com>
2017-03-30 09:21:46 -0700

    Add persistence service to our project #123
    Added interfaces and impls for persistence

---
+++ Common/src/main/java/UtilsImplementations/PersistenceType.java
@@ -0,0 +1,12 @@
+package UtilsImplementations;
+
+
+/** PersistenceType - The type use for store
+ * @author Shimon Azulay
+ * @since 2016-03-30*/
+
+public class PersistenceType { 
+	
+	public static final String XML = "xml";
+	
+}
