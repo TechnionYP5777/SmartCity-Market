74d6e0c74bde2ad8db76b3aed5eb37649141496a
Idan Atias <idan2k3@gmail.com>
2017-03-31 00:40:30 +0300

    Changed LoginTest of Customer to fit new login mechanism as part of #332
    Signed-off-by: Idan Atias <idan2k3@gmail.com>

--- Customer/src/test/java/CustomerImplemantationsTests/LoginTest.java
+++ Customer/src/test/java/CustomerImplemantationsTests/LoginTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import BasicCommonClasses.CustomerProfile;
 import BasicCommonClasses.Login;
 import ClientServerApi.CommandDescriptor;
 import ClientServerApi.CommandWrapper;
@@ -26,6 +27,7 @@ import UtilsImplementations.Serialization;
 
 /**
  * @author Lior Ben Ami
+ * @author idan atias
  * @since 2017-03-25
  */
 
@@ -47,15 +49,15 @@ public class LoginTest {
 		try {
 			Mockito.when(
 				clientRequestHandler.sendRequestWithRespond((new CommandWrapper(CustomerDefs.loginCommandSenderId,
-						CommandDescriptor.LOGIN, Serialization.serialize(new Login("test", "test"))).serialize())))
-				.thenReturn(new CommandWrapper(ResultDescriptor.SM_OK).serialize());
+						CommandDescriptor.LOGIN_CLIENT, Serialization.serialize(new Login("Guest", "Guest"))).serialize())))
+				.thenReturn(new CommandWrapper(ResultDescriptor.SM_OK, Serialization.serialize(new CustomerProfile("Guest"))).serialize());
 			
 		} catch (IOException ¢) {
 			¢.printStackTrace();
 			fail();
 		}
 		try {
-			customer.login("test", "test");
+			customer.login("Guest", "Guest");
 		} catch (CriticalError | AuthenticationError ¢) {
 			¢.printStackTrace();
 			fail();
@@ -70,14 +72,14 @@ public class LoginTest {
 		try {
 			Mockito.when(
 					clientRequestHandler.sendRequestWithRespond((new CommandWrapper(CustomerDefs.loginCommandSenderId,
-							CommandDescriptor.LOGIN, Serialization.serialize(new Login("test", "test"))).serialize())))
+							CommandDescriptor.LOGIN_CLIENT, Serialization.serialize(new Login("Guest", "Guest"))).serialize())))
 					.thenReturn(new CommandWrapper(ResultDescriptor.SM_ERR).serialize());
 		} catch (IOException ¢) {
 			¢.printStackTrace();
 			fail();
 		}
 		try {
-			customer.login("test", "test");
+			customer.login("Guest", "Guest");
 		} catch (AuthenticationError ¢) {
 			¢.printStackTrace();
 			fail();
@@ -87,18 +89,39 @@ public class LoginTest {
 	}
 	
 	@Test
-	public void AuthenticationError() {
+	public void AuthenticationErrorPasswordIncorrect() {
 		try {
 			Mockito.when(
 					clientRequestHandler.sendRequestWithRespond((new CommandWrapper(CustomerDefs.loginCommandSenderId,
-							CommandDescriptor.LOGIN, Serialization.serialize(new Login("test", "test"))).serialize())))
+							CommandDescriptor.LOGIN_CLIENT, Serialization.serialize(new Login("Guest", "GuesT"))).serialize())))
 					.thenReturn(new CommandWrapper(ResultDescriptor.SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD).serialize());
 		} catch (IOException ¢) {
 			¢.printStackTrace();
 			fail();
 		}
 		try {
-			customer.login("test", "test");
+			customer.login("Guest", "GuesT");
+		} catch (CriticalError ¢) {
+			¢.printStackTrace();
+			fail();
+		} catch (AuthenticationError ¢) {
+		/* Test Passed */
+		}
+	}
+	
+	@Test
+	public void AuthenticationErrorUserNameIncorrect() {
+		try {
+			Mockito.when(
+					clientRequestHandler.sendRequestWithRespond((new CommandWrapper(CustomerDefs.loginCommandSenderId,
+							CommandDescriptor.LOGIN_CLIENT, Serialization.serialize(new Login("Guesttt", "Guest"))).serialize())))
+					.thenReturn(new CommandWrapper(ResultDescriptor.SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD).serialize());
+		} catch (IOException ¢) {
+			¢.printStackTrace();
+			fail();
+		}
+		try {
+			customer.login("Guesttt", "Guest");
 		} catch (CriticalError ¢) {
 			¢.printStackTrace();
 			fail();
