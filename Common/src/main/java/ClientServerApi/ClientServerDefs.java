8c284cf84d44206c418ef083a20a006233173307
noam yefet <noamyft@gmail.com>
2017-04-09 01:19:20 +0300

    Common: add ClientServerDefs class that contains the definition of the anonymous      usernamr and password.
    Related issue:#362
    
    Signed-off-by: noam yefet <noamyft@gmail.com>

---
+++ Common/src/main/java/ClientServerApi/ClientServerDefs.java
@@ -0,0 +1,11 @@
+package ClientServerApi;
+
+public class ClientServerDefs {
+	
+	/*
+	 * username and password for anonymous client login
+	 */
+	public static String anonymousCustomerUsername = "Guest";
+	public static String anonymousCustomerPassword = anonymousCustomerUsername;
+	
+}
