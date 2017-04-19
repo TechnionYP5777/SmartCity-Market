88e316d3a759f72067efa65cea13bd7cf43eb594
Aviad Cohen <aviadcohen7@gmail.com>
2017-04-19 14:21:27 +0300

    Customer: Used Guest defination from ClientServerAPI
    Related issues:
    
    Replace "Cart" username to "Guest" #362
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- Customer/src/main/java/CustomerGuiScreens/CustomerLoginScreen.java
+++ Customer/src/main/java/CustomerGuiScreens/CustomerLoginScreen.java
@@ -16,14 +16,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 
 import com.jfoenix.controls.JFXTextField;
 
 import BasicCommonClasses.Login;
+import ClientServerApi.ClientServerDefs;
 
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXButton;;
@@ -39,7 +38,7 @@ public class CustomerLoginScreen implements Initializable {
 	
 	private String username = "";
 	private String password = "";
-	private static Login guestLogin = new Login("Guest", "Guest");
+	private static Login guestLogin = new Login(ClientServerDefs.anonymousCustomerUsername, ClientServerDefs.anonymousCustomerUsername);
 	@FXML
 	private GridPane loginScreenPane;
 	@FXML
