96bf0e989a604eb5b970ed2af0439e45d91a92ae
shimonAzulay <sazulay3@gmail.com>
2017-05-15 07:57:04 -0700

    Add to manager GUI employees manager tab #308
    add the employee list, search and remove

--- Employee/src/main/java/EmployeeGui/ManageEmployeesTab.java
+++ Employee/src/main/java/EmployeeGui/ManageEmployeesTab.java
@@ -2,6 +2,7 @@ package EmployeeGui;
 
 import java.net.URL;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.ResourceBundle;
 import java.util.stream.IntStream;
 
@@ -22,13 +23,15 @@ import EmployeeDefs.AEmployeeException.CriticalError;
 import EmployeeDefs.AEmployeeException.EmployeeNotConnected;
 import EmployeeDefs.AEmployeeException.InvalidParameter;
 import EmployeeDefs.AEmployeeException.WorkerAlreadyExists;
+import EmployeeDefs.AEmployeeException.WorkerDoesNotExist;
 import EmployeeImplementations.Manager;
 import GuiUtils.RadioButtonEnabler;
 import UtilsImplementations.InjectionFactory;
 import de.jensd.fx.glyphs.GlyphsBuilder;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
-import javafx.beans.value.ChangeListener;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -36,10 +39,9 @@ import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.util.Callback;
 
 /**
  * ManageEmployeesTab - manages the employee tab
@@ -50,61 +52,46 @@ import javafx.scene.layout.VBox;
 public class ManageEmployeesTab implements Initializable {
 
 	@FXML
-	private JFXListView<String> employeesList;
+    private JFXListView<String> employeesList;
 
-	@FXML
-	private JFXTextField searchEmployee;
+    @FXML
+    private JFXTextField searchEmployee;
 
-	@FXML
-	private StackPane stackPane;
+    @FXML
+    private JFXTextField userTxt;
 
-	@FXML
-	private VBox waitingPane;
+    @FXML
+    private JFXPasswordField passTxt;
 
-	@FXML
-	private VBox detailsPane;
+    @FXML
+    private JFXComboBox<String> securityCombo;
 
-	@FXML
-	private Label emplyeeTitleLbl;
+    @FXML
+    private JFXTextField securityAnswerTxt;
 
-	@FXML
-	private Label emplyeeNameLbl;
+    @FXML
+    private JFXRadioButton workerRadioBtn;
 
-	@FXML
-	private Label employeeUser;
-
-	@FXML
-	private JFXButton removeBtn;
-
-	@FXML
-	private JFXTextField nameTxt;
-
-	@FXML
-	private JFXTextField userTxt;
-
-	@FXML
-	private JFXPasswordField passTxt;
-
-	@FXML
-	private JFXComboBox<String> securityCombo;
-
-	@FXML
-	private JFXTextField securityAnswerTxt;
-
-	@FXML
-	private JFXRadioButton workerRadioBtn;
-
-	@FXML
-	private JFXRadioButton managerRadioBtn;
-
-	@FXML
-	private JFXButton finishBtn;
+    @FXML
+    private JFXRadioButton managerRadioBtn;
+    
+    @FXML
+    private JFXButton finishBtn;
+    
+    @FXML
+    private JFXButton removeEmployeesBtn;
 
 	RadioButtonEnabler radioBtnCont = new RadioButtonEnabler();
 
 	IManager manager = InjectionFactory.getInstance(Manager.class);
 
 	static Logger log = Logger.getLogger(ManageEmployeesTab.class.getName());
+	
+	private HashSet<String> selectedEmployees = new HashSet<String>();
+	
+	private ObservableList<String> dataEmployees;
+	
+	private FilteredList<String> filteredDataEmployees;
 	
 //	private HashMap<String,>
 	
@@ -115,15 +102,15 @@ public class ManageEmployeesTab implements Initializable {
 		try {
 			manager.registerNewWorker(new Login(userTxt.getText(), passTxt.getText()));
 		} catch (InvalidParameter | CriticalError | EmployeeNotConnected | ConnectionFailure | WorkerAlreadyExists e) {
-			log.fatal(e.getStackTrace() + "\n" + e.getStackTrace() + "\n" + e.getMessage());
+			log.debug(e.getStackTrace());
+			log.fatal(e.getMessage());
 		}
+		createEmployeesList();
+		enableRemoveButton();
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		nameTxt.textProperty().addListener((observable, oldValue, newValue) -> {
-			enableFinishBtn();
-		});
 
 		userTxt.textProperty().addListener((observable, oldValue, newValue) -> {
 			enableFinishBtn();
@@ -141,17 +128,6 @@ public class ManageEmployeesTab implements Initializable {
 
 		securityCombo.getItems().addAll("Q1", "Q2", "Q3", "Q4");
 
-		RequiredFieldValidator validator = new RequiredFieldValidator();
-		validator.setMessage("Input Required");
-		validator.setIcon(GlyphsBuilder.create(FontAwesomeIconView.class).glyph(FontAwesomeIcon.WARNING).size("1em")
-				.styleClass("error").build());
-
-		nameTxt.getValidators().add(validator);
-		nameTxt.focusedProperty().addListener((o, oldVal, newVal) -> {
-			if (!newVal) {
-				nameTxt.validate();
-			}
-		});
 
 		RequiredFieldValidator validator2 = new RequiredFieldValidator();
 		validator2.setMessage("Input Required");
@@ -191,52 +167,59 @@ public class ManageEmployeesTab implements Initializable {
 
 		createEmployeesList();
 		
-		employeesList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-
-		    @Override
-		    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-		    	employeeSelected(newValue);
-		    }
+		searchEmployee.textProperty().addListener(obs -> {
+			String filter = searchEmployee.getText();
+			if (filter == null || filter.length() == 0) {
+				filteredDataEmployees.setPredicate(s -> true);
+			} else {
+				filteredDataEmployees.setPredicate(s -> s.contains(filter));
+			}
 		});
+		
+		employeesList.setCellFactory(CheckBoxListCell.forListView(new Callback<String, ObservableValue<Boolean>>() {
+			@Override
+			public ObservableValue<Boolean> call(String item) {
+				BooleanProperty observable = new SimpleBooleanProperty();
+				observable.set(selectedEmployees.contains(item));
 
+				observable.addListener((obs, wasSelected, isNowSelected) -> {
+					if (isNowSelected) {
+						selectedEmployees.add(item);
+					} else {
+						selectedEmployees.remove(item);
+					}
+					enableRemoveButton();
+
+				});
+				return observable;
+			}
+		}));
+		
 		enableFinishBtn();
-
+		enableRemoveButton();
 	}
 	
-	private void employeeSelected(String employee) {
+	private void enableRemoveButton() {
+		removeEmployeesBtn.setDisable(/* TODO check if the selected is now connected and */ selectedEmployees.isEmpty());
 		
 	}
+	
 
 	private void createEmployeesList() {
 		
-		ObservableList<String> data = FXCollections.observableArrayList();
-		
-	    IntStream.range(0, 1000).mapToObj(Integer::toString).forEach(data::add);
-	    
-	    try {
-			manager.getAllWorkers();
-		} catch (CriticalError | EmployeeNotConnected | ConnectionFailure e) {
-			log.fatal(e.getStackTrace() + "\n" + e.getMessage());
-		}
+		selectedEmployees = new HashSet<String>();
 
-	    FilteredList<String> filteredData = new FilteredList<>(data, s -> true);
+		dataEmployees = FXCollections.observableArrayList();
 
-	    searchEmployee.textProperty().addListener(obs->{
-	        String filter = searchEmployee.getText(); 
-	        if(filter == null || filter.length() == 0) {
-	            filteredData.setPredicate(s -> true);
-	        }
-	        else {
-	            filteredData.setPredicate(s -> s.contains(filter));
-	        }
-	    });
+		IntStream.range(0, 1000).mapToObj(Integer::toString).forEach(dataEmployees::add);
 
-	    employeesList.setItems(filteredData);
-	    
+		filteredDataEmployees = new FilteredList<>(dataEmployees, s -> true);
+
+		employeesList.setItems(filteredDataEmployees);    
 	}
 
 	private void enableFinishBtn() {
-		finishBtn.setDisable(nameTxt.getText().isEmpty() || userTxt.getText().isEmpty() || passTxt.getText().isEmpty()
+		finishBtn.setDisable(userTxt.getText().isEmpty() || passTxt.getText().isEmpty()
 				|| securityAnswerTxt.getText().isEmpty());
 	}
 
@@ -248,7 +231,17 @@ public class ManageEmployeesTab implements Initializable {
 
 	@FXML
 	void removeBtnPressed(ActionEvent event) {
-
+		selectedEmployees.forEach(eml -> {
+			try {
+				manager.removeWorker(eml);
+			} catch (InvalidParameter | CriticalError | EmployeeNotConnected | ConnectionFailure
+					| WorkerDoesNotExist e) {
+				log.debug(e.getStackTrace());
+				log.fatal(e.getMessage());		
+			}
+		});
+		createEmployeesList();
+		enableRemoveButton();
 	}
 
 }
