7560e9a8c415aaa4dc0daab29ef8cc38f93bc831
Aviad Cohen <aviadcohen7@gmail.com>
2017-05-27 09:46:20 +0300

    Remove old LOGIN command code all over the project when refactoring finish
    Related issues:
    
    Remove old LOGIN command code all over the project when refactoring finish #325
    
    Signed-off-by: Aviad Cohen <aviadcohen7@gmail.com>

--- Server/src/main/java/CommandHandler/CommandExecuter.java
+++ Server/src/main/java/CommandHandler/CommandExecuter.java
@@ -65,64 +65,6 @@ public class CommandExecuter {
 		}
 	}
 
-	//TODO Aviad - remove login command and splitting login command finished
-	@Deprecated
-	private void loginCommand(SQLDatabaseConnection c) {
-		Login login = null;
-
-		log.info("Login command called");
-
-		try {
-			login = Serialization.deserialize(inCommandWrapper.getData(), Login.class);
-		} catch (java.lang.RuntimeException e) {
-			log.fatal("Failed to parse data for login command");
-
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_ERR);
-
-			return;
-		}
-
-		if (!login.isValid()) {
-			log.info("Login command failed, username and password can't be empty");
-
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_INVALID_PARAMETER);
-
-			return;
-		}
-
-		try {
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_OK);
-			outCommandWrapper.setSenderID(c.login(login.getUserName(), login.getPassword()));
-
-			try {
-				outCommandWrapper.setData(c.getClientType(outCommandWrapper.getSenderID()));
-			} catch (ClientNotConnected e) {
-				log.fatal("Client is not connected for sender ID " + outCommandWrapper.getSenderID());
-			}
-
-			log.info("Login command succeded with sender ID " + outCommandWrapper.getSenderID() + " with client type "
-					+ outCommandWrapper.getData());
-		} catch (AuthenticationError e) {
-			log.info("Login command failed, username dosen't exist or wrong password received");
-
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_USERNAME_DOES_NOT_EXIST_WRONG_PASSWORD);
-		} catch (CriticalError e) {
-			log.fatal("Login command failed, critical error occured from SQL Database connection");
-
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_ERR);
-		} catch (ClientAlreadyConnected e) {
-			log.info("Login command failed, user already connected");
-
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_SENDER_IS_ALREADY_CONNECTED);
-		} catch (NumberOfConnectionsExceeded e) {
-			log.fatal("Login command failed, too much connections (try to increase TRYS_NUMBER)");
-
-			outCommandWrapper = new CommandWrapper(ResultDescriptor.SM_ERR);
-		}
-
-		log.info("Login with User " + login.getUserName() + " finished");
-	}
-
 	private void loginEmployeeCommand(SQLDatabaseConnection c) {
 		Login login = null;
 
@@ -1524,11 +1466,6 @@ public class CommandExecuter {
 		}
 
 		switch (inCommandWrapper.getCommandDescriptor()) {
-		case LOGIN:
-			loginCommand(c);
-
-			break;
-			
 		case LOGIN_EMPLOYEE:
 			loginEmployeeCommand(c);
 
