c16ab1b14e8cce7e10d1efd2ba4061c83363b492
shimonAzulay <sazulay3@gmail.com>
2017-05-31 10:57:14 -0700

    Bugs in customer registration flow #441
    enbale next button

--- Customer/src/main/java/CustomerGuiScreens/CustomerRegistration_PersonalInfoScreen.java
+++ Customer/src/main/java/CustomerGuiScreens/CustomerRegistration_PersonalInfoScreen.java
@@ -1,6 +1,7 @@
 package CustomerGuiScreens;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
@@ -29,124 +30,168 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.GridPane;
 
-
 /**
- * CustomerRegistration_PersonalInfoScreen - Controller for the first screen of the customer registration screens.
- * 											This screen handles the username, pass and personal info.
+ * CustomerRegistration_PersonalInfoScreen - Controller for the first screen of
+ * the customer registration screens. This screen handles the username, pass and
+ * personal info.
  * 
  * @author Lior Ben Ami
  * @since 2017-04
  */
 public class CustomerRegistration_PersonalInfoScreen implements Initializable {
-	
+
 	protected static Logger log = Logger.getLogger(CustomerRegistration_PersonalInfoScreen.class.getName());
-	
+
 	@FXML
-	private GridPane personalInfoScreenPane; 
+	private GridPane personalInfoScreenPane;
+
+	@FXML
+	private JFXTextField userNameTextField;
+
+	@FXML
+	private JFXPasswordField passwordField;
+
+	@FXML
+	private JFXPasswordField repeatPassField;
+
+	@FXML
+	private JFXTextField firstNameTextField;
+
+	@FXML
+	JFXTextField lastNameTextField;
+
+	@FXML
+	private JFXTextField phoneNumberTextField;
+
+	@FXML
+	private JFXButton nextButton;
+
+	@FXML
+	private JFXTextField emailTextField;
+
+	@FXML
+	private JFXTextField cityTextField;
+
+	@FXML
+	private JFXTextField streetTextField;
+
+	@FXML
+	private JFXDatePicker birthDatePicker;
+
+	@FXML
+	private JFXComboBox<String> securityQuestionComboBox;
+
+	@FXML
+	private JFXTextField securityAnswerTextField;
+
+	ICustomerProfile customerProfile;
+
+	ICustomer customer;
 	
-    @FXML
-    private JFXTextField userNameTextField;
+	private void enableNextButton() {
+		nextButton.setDisable(userNameTextField.getText().isEmpty() || passwordField.getText().isEmpty()
+				|| repeatPassField.getText().isEmpty() || firstNameTextField.getText().isEmpty()
+				|| lastNameTextField.getText().isEmpty() || phoneNumberTextField.getText().isEmpty()
+				|| emailTextField.getText().isEmpty() || cityTextField.getText().isEmpty()
+				|| streetTextField.getText().isEmpty() || securityAnswerTextField.getText().isEmpty() || 
+				securityQuestionComboBox.getSelectionModel().getSelectedItem().toString().isEmpty());
+	}
 
-    @FXML
-    private JFXPasswordField passwordField;
-    
-    @FXML
-    private JFXPasswordField repeatPassField;
-    
-    @FXML
-    private JFXTextField firstNameTextField;
-    
-    @FXML
-    JFXTextField lastNameTextField;
-    
-    @FXML
-    private JFXTextField phoneNumberTextField;
 
-    @FXML
-    private JFXButton nextButton;
-
-    @FXML
-    private JFXTextField emailTextField;
-
-    @FXML
-    private JFXTextField cityTextField;
-
-    @FXML
-    private JFXTextField streetTextField;
-    
-    @FXML
-    private JFXDatePicker birthDatePicker;
-    
-    @FXML
-    private JFXComboBox<String> securityQuestionComboBox;
-    
-    @FXML
-    private JFXTextField securityAnswerTextField;
-    
-    ICustomerProfile customerProfile ;
-    
-    ICustomer customer;
-    
 	@Override
 	public void initialize(URL location, ResourceBundle __) {
-		
+
 		AbstractApplicationScreen.fadeTransition(personalInfoScreenPane);
-		
+
 		updateFields();
-		
-		
-		customer = InjectionFactory.getInstance(Customer.class);	
-		
+
+		customer = InjectionFactory.getInstance(Customer.class);
+
 		userNameTextField.textProperty().addListener((observable, oldValue, newValue) -> {
 			if (validNewUserName(newValue))
 				TempCustomerProfilePassingData.customerProfile.setUserName(newValue);
 			else
 				DialogMessagesService.showErrorDialog(GuiCommonDefs.registrationFieldFailureTitle, null,
 						GuiCommonDefs.registrationUsedUserName);
-				
+
 		});
 
-		passwordField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.password = newValue);
-		
-		firstNameTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setFirstName(newValue));
-		
-		lastNameTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setLastName(newValue));
-		
-		phoneNumberTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setPhoneNumber(newValue));
-		
-		emailTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setEmailAddress(newValue));		
-		
-		cityTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setCity(newValue));
-		
-		streetTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setStreet(newValue));
-		
-		birthDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.customerProfile.setBirthdate(newValue));
-	 
-		securityQuestionComboBox.valueProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.sequrityQuestion = newValue);
-		
-		securityQuestionComboBox.setButtonCell(new ListCell<String>(){
+		passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.password = newValue;
+			enableNextButton();
 
-	        @Override
-	        protected void updateItem(String item, boolean empty) {
-	            super.updateItem(item, empty); 
-	            if(empty || item==null){
-	                setStyle(null);
-	            	setText(null);
-	            } else {
-	                setStyle("-fx-text-fill: #4d4d4d");
-	                setText(item.toString());
-	            }
-	        }
+		});
 
-	    });
+		firstNameTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setFirstName(newValue);
+			enableNextButton();
+		});
+
+		lastNameTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setLastName(newValue);
+			enableNextButton();
+		});
+
+		phoneNumberTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setPhoneNumber(newValue);
+			enableNextButton();
+		});
+
+		emailTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setEmailAddress(newValue);
+			enableNextButton();
+		});
+
+		cityTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setCity(newValue);
+			enableNextButton();
+		});
+
+		streetTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setStreet(newValue);
+			enableNextButton();
+		});
+
+		birthDatePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.customerProfile.setBirthdate(newValue);
+			enableNextButton();
+		});
+
+		securityQuestionComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.sequrityQuestion = newValue;
+			enableNextButton();
+		});
+
+		securityQuestionComboBox.setButtonCell(new ListCell<String>() {
+
+			@Override
+			protected void updateItem(String item, boolean empty) {
+				super.updateItem(item, empty);
+				if (empty || item == null) {
+					setStyle(null);
+					setText(null);
+				} else {
+					setText(item.toString());
+				}
+			}
+		});
+
+		securityAnswerTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+			TempCustomerProfilePassingData.sequrityAnswer = newValue;
+			enableNextButton();
+		});
 		
-		securityAnswerTextField.textProperty().addListener((observable, oldValue, newValue) -> TempCustomerProfilePassingData.sequrityAnswer = newValue);
+		
+		birthDatePicker.setValue(LocalDate.now());
+		
+		enableNextButton();
+		
 	}
 
 	private boolean validNewUserName(String username) {
 		Boolean res = false;
 		try {
-			res= customer.isFreeUsername(username);
+			res = customer.isFreeUsername(username);
 		} catch (SMException e) {
 			log.fatal(e);
 			log.debug(StackTraceUtil.getStackTrace(e));
@@ -161,8 +206,8 @@ public class CustomerRegistration_PersonalInfoScreen implements Initializable {
 			TempCustomerProfilePassingData.customerProfile = InjectionFactory.getInstance(CustomerProfile.class);
 			return;
 		}
-		//else
-		
+		// else
+
 		if (TempCustomerProfilePassingData.customerProfile.getUserName() != null)
 			userNameTextField.setText(TempCustomerProfilePassingData.customerProfile.getUserName());
 		if (TempCustomerProfilePassingData.password != null) {
@@ -171,49 +216,50 @@ public class CustomerRegistration_PersonalInfoScreen implements Initializable {
 		}
 		if (TempCustomerProfilePassingData.customerProfile.getFirstName() != null)
 			firstNameTextField.setText(TempCustomerProfilePassingData.customerProfile.getFirstName());
-		
+
 		if (TempCustomerProfilePassingData.customerProfile.getLastName() != null)
 			lastNameTextField.setText(TempCustomerProfilePassingData.customerProfile.getLastName());
-		
+
 		if (TempCustomerProfilePassingData.customerProfile.getPhoneNumber() != null)
 			phoneNumberTextField.setText(TempCustomerProfilePassingData.customerProfile.getPhoneNumber());
-		
+
 		if (TempCustomerProfilePassingData.customerProfile.getEmailAddress() != null)
 			emailTextField.setText(TempCustomerProfilePassingData.customerProfile.getEmailAddress());
 
 		if (TempCustomerProfilePassingData.customerProfile.getCity() != null)
 			cityTextField.setText(TempCustomerProfilePassingData.customerProfile.getCity());
-		
+
 		if (TempCustomerProfilePassingData.customerProfile.getStreet() != null)
 			streetTextField.setText(TempCustomerProfilePassingData.customerProfile.getStreet());
-		
+
 		if (TempCustomerProfilePassingData.customerProfile.getBirthdate() != null)
 			birthDatePicker.valueProperty().setValue(TempCustomerProfilePassingData.customerProfile.getBirthdate());
-		
-		if (TempCustomerProfilePassingData.sequrityQuestion!= null);
-			securityQuestionComboBox.setValue(TempCustomerProfilePassingData.sequrityQuestion);
-			
-		if (TempCustomerProfilePassingData.sequrityAnswer!= null)
+
+		if (TempCustomerProfilePassingData.sequrityQuestion != null)
+			;
+		securityQuestionComboBox.setValue(TempCustomerProfilePassingData.sequrityQuestion);
+
+		if (TempCustomerProfilePassingData.sequrityAnswer != null)
 			securityAnswerTextField.setText(TempCustomerProfilePassingData.sequrityAnswer);
 	}
-	
+
 	private void getSecurityQuestions() {
 		securityQuestionComboBox.getItems().addAll(SecurityQuestions.getQuestions());
 	}
 
 	@FXML
-    void nextButtonPressed(ActionEvent __) {
-	  	if (checkFieldsVadility())
-			AbstractApplicationScreen.setScene("/CustomerRegistrationScreens/CustomerRegistration_IngredientsScreen.fxml");
-  	}
-	
+	void nextButtonPressed(ActionEvent __) {
+		if (checkFieldsVadility())
+			AbstractApplicationScreen
+					.setScene("/CustomerRegistrationScreens/CustomerRegistration_IngredientsScreen.fxml");
+	}
+
 	@FXML
-    void cancelButtonPressed(ActionEvent __) {
+	void cancelButtonPressed(ActionEvent __) {
 		TempCustomerProfilePassingData.clear();
 		AbstractApplicationScreen.setScene("/CustomerLoginScreen/CustomerLoginScreen.fxml");
 	}
 
-	
 	private boolean checkFieldsVadility() {
 		if (passwordField.getText().equals(repeatPassField.getText()))
 			return true;
@@ -221,4 +267,5 @@ public class CustomerRegistration_PersonalInfoScreen implements Initializable {
 				GuiCommonDefs.registrationWrongRepeatedPass);
 		return false;
 	}
+
 }
