bb133da30526ed0e2bbe1a9c61e7a145d05b3400
noam yefet <noamyft@gmail.com>
2017-06-29 12:11:08 +0300

    Sales: Add property: LastPopularityProductProperty
        Related issue: #517
    
    Signed-off-by: noam yefet <noamyft@gmail.com>

---
+++ Sales/src/main/java/ml/common/property/basicproperties/storestatistics/LastPopularProductProperty.java
@@ -0,0 +1,58 @@
+package ml.common.property.basicproperties.storestatistics;
+
+import api.contracts.IProduct;
+import ml.common.property.basicproperties.ABasicProperty;
+import ml.deducer.deductionrules.ADeductionRule;
+
+public class LastPopularProductProperty extends ABasicProperty {
+public static int numOfBottom = 50;
+	
+	long amount;
+	IProduct product;
+	
+	
+	public LastPopularProductProperty(IProduct product, long amount) {
+		super();
+		this.amount = amount;
+		this.product = product;
+	}
+	
+	public LastPopularProductProperty(IProduct product, long amount, ADeductionRule rule) {
+		super(rule);
+		this.amount = amount;
+		this.product = product;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public IProduct getProduct() {
+		return product;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LastPopularProductProperty other = (LastPopularProductProperty) obj;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+}
