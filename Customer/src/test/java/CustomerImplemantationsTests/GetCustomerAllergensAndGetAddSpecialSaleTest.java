8884661d1508b81dea870fac8c9bd431141998f2
LiorBenAmi <benamilior89@gmail.com>
2017-06-30 21:33:26 +0300

    testing registered ustomer #522

---
+++ Customer/src/test/java/CustomerImplemantationsTests/GetCustomerAllergensAndGetAddSpecialSaleTest.java
@@ -0,0 +1,86 @@
+package CustomerImplemantationsTests;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import BasicCommonClasses.CustomerProfile;
+import BasicCommonClasses.Ingredient;
+import BasicCommonClasses.Sale;
+import ClientServerApi.CommandDescriptor;
+import ClientServerApi.CommandWrapper;
+import ClientServerApi.ResultDescriptor;
+import CustomerContracts.IRegisteredCustomer;
+import CustomerContracts.ACustomerExceptions.AuthenticationError;
+import CustomerContracts.ACustomerExceptions.CustomerNotConnected;
+import CustomerContracts.ACustomerExceptions.InvalidParameter;
+import CustomerImplementations.CustomerDefs;
+import CustomerImplementations.RegisteredCustomer;
+import SMExceptions.CommonExceptions.CriticalError;
+import UtilsContracts.IClientRequestHandler;
+import UtilsImplementations.Serialization;
+
+/** 
+ * @author Lior Ben Ami
+ * @since 2017-06-30
+ * */
+@RunWith(MockitoJUnitRunner.class)
+public class GetCustomerAllergensAndGetAddSpecialSaleTest {
+
+	private IRegisteredCustomer customer;
+	long barcode = 1234567;
+	Sale sale = new Sale(1, barcode, 3, 22.0), testSale = null;
+	
+	static String username = "username";
+	static CustomerProfile customerProfile = new CustomerProfile(username);
+	HashSet<Ingredient> allergens = new  HashSet<Ingredient>(Arrays.asList(new Ingredient(1, "milk"), new Ingredient(2, "meat"))),
+			testAllergens = null;
+	@Mock
+	private IClientRequestHandler clientRequestHandler;
+
+	@Before
+	public void setup() {
+		PropertyConfigurator.configure("../log4j.properties");
+		customer = new RegisteredCustomer(clientRequestHandler);
+	}
+	
+	@Test
+	public void getCustomerAllergansTest() {	
+		customerProfile.setAllergens(allergens);
+		
+		try {
+			Mockito.when(
+				clientRequestHandler.sendRequestWithRespond(new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.UPDATE_CUSTOMER_PROFILE,
+						Serialization.serialize(customerProfile)).serialize()))
+				.thenReturn(new CommandWrapper(ResultDescriptor.SM_OK).serialize());
+		} catch (IOException ¢) {
+			fail();
+		}
+		
+		try {
+			customer.updateCustomerProfile(customerProfile);
+		} catch (InvalidParameter | AuthenticationError | CriticalError | CustomerNotConnected e) {
+			fail();
+		}
+		testAllergens = customer.getCustomerAllergens();
+		assert(testAllergens.equals(allergens));
+	}
+	
+	@Test
+	public void addAndGetSpecialSalesTest() {	
+		customer.addSpecialSale(sale, false);
+		HashMap<Sale,Boolean> sales = customer.getSpecialSales();
+		assert(sales.containsKey(sale));
+	}
+}
--- Customer/src/test/java/CustomerImplemantationsTests/OfferSpecailSaleForProductTest.java
+++ Customer/src/test/java/CustomerImplemantationsTests/OfferSpecailSaleForProductTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.junit.Before;
@@ -13,73 +15,80 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import BasicCommonClasses.CustomerProfile;
+import BasicCommonClasses.Ingredient;
 import BasicCommonClasses.Sale;
 import ClientServerApi.CommandDescriptor;
 import ClientServerApi.CommandWrapper;
 import ClientServerApi.ResultDescriptor;
-import CustomerContracts.ICustomer;
+import CustomerContracts.IRegisteredCustomer;
 import CustomerContracts.ACustomerExceptions.CustomerNotConnected;
 import CustomerContracts.ACustomerExceptions.InvalidParameter;
 import CustomerContracts.ACustomerExceptions.ProductCatalogDoesNotExist;
-import CustomerImplementations.Customer;
 import CustomerImplementations.CustomerDefs;
+import CustomerImplementations.RegisteredCustomer;
 import SMExceptions.CommonExceptions.CriticalError;
 import UtilsContracts.IClientRequestHandler;
 import UtilsImplementations.Serialization;
 
-/**
+/** 
  * @author Lior Ben Ami
- * @since  2017-06-30
- */
+ * @since 2017-06-30
+ * */
 @RunWith(MockitoJUnitRunner.class)
-public class GetSalesForProductTest {
-
-	private ICustomer customer;
+public class OfferSpecailSaleForProductTest {
+private IRegisteredCustomer customer;
 	
 	long barcode = 1234567;
 	Sale sale = new Sale(1, barcode, 3, 22.0), testSale = null;
 	
+	static String username = "username";
+	static CustomerProfile customerProfile = new CustomerProfile(username);
+	HashSet<Ingredient> allergens = new  HashSet<Ingredient>(Arrays.asList(new Ingredient(1, "milk"), new Ingredient(2, "meat"))),
+			testAllergens = null;
 	@Mock
 	private IClientRequestHandler clientRequestHandler;
 
 	@Before
 	public void setup() {
 		PropertyConfigurator.configure("../log4j.properties");
-		customer = new Customer(clientRequestHandler);
+		customer = new RegisteredCustomer(clientRequestHandler);
 	}
 	
 	@Test
-	public void GetSaleForProductSuccessfulTest() {
+	public void OfferSpecailSaleForProductSuccessfulTest() {	
+		
 		try {
-			Mockito.when(clientRequestHandler.sendRequestWithRespond(
-					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.GET_SALE_FOR_PRODUCT,
-							Serialization.serialize(barcode)).serialize()))
-					.thenReturn(new CommandWrapper(ResultDescriptor.SM_OK, Serialization.serialize(sale)).serialize());
+			Mockito.when(
+				clientRequestHandler.sendRequestWithRespond(new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.OFFER_SPECIAL_SALE_FOR_PRODUCT,
+						Serialization.serialize(sale)).serialize()))
+				.thenReturn(new CommandWrapper(ResultDescriptor.SM_OK, Serialization.serialize(sale)).serialize());
 		} catch (IOException ¢) {
 			
 			fail();
 		}
 		try {
-			testSale = customer.getSaleForProduct(barcode);
+			testSale = customer.offerSpecailSaleForProduct(sale);
 		} catch (CriticalError | CustomerNotConnected | ProductCatalogDoesNotExist | InvalidParameter e) {
 			fail();
 		}
 		assertEquals(testSale, sale);
 	}
 	
+
 	@Test
-	public void GetSaleForProductCriticalErrorTest() {
+	public void OfferSpecailSaleForProductCriticalErrorTest() {
 		try {
 			Mockito.when(clientRequestHandler.sendRequestWithRespond(
-					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.GET_SALE_FOR_PRODUCT,
-							Serialization.serialize(barcode)).serialize()))
+					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.OFFER_SPECIAL_SALE_FOR_PRODUCT,
+							Serialization.serialize(sale)).serialize()))
 					.thenReturn(new CommandWrapper(ResultDescriptor.SM_GROCERY_LIST_IS_EMPTY, Serialization.serialize(sale)).serialize());
 		} catch (IOException ¢) {
 			
 			fail();
 		}
 		try {
-			customer.getSaleForProduct(barcode);
+			customer.offerSpecailSaleForProduct(sale);
 		} catch ( CustomerNotConnected | ProductCatalogDoesNotExist | InvalidParameter e) {
 			fail();
 		} catch (CriticalError e) {
@@ -88,18 +97,18 @@ public class GetSalesForProductTest {
 	}
 	
 	@Test
-	public void GetSaleForProductCustomerNotConnectedTest() {
+	public void OfferSpecailSaleForProductCustomerNotConnectedTest() {
 		try {
 			Mockito.when(clientRequestHandler.sendRequestWithRespond(
-					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.GET_SALE_FOR_PRODUCT,
-							Serialization.serialize(barcode)).serialize()))
+					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.OFFER_SPECIAL_SALE_FOR_PRODUCT,
+							Serialization.serialize(sale)).serialize()))
 					.thenReturn(new CommandWrapper(ResultDescriptor.SM_SENDER_IS_NOT_CONNECTED, Serialization.serialize(sale)).serialize());
 		} catch (IOException ¢) {
 			
 			fail();
 		}
 		try {
-			customer.getSaleForProduct(barcode);
+			customer.offerSpecailSaleForProduct(sale);
 		} catch ( CriticalError | ProductCatalogDoesNotExist | InvalidParameter e) {
 			fail();
 		} catch ( CustomerNotConnected e) {
@@ -108,18 +117,18 @@ public class GetSalesForProductTest {
 	}
 	
 	@Test
-	public void GetSaleForProductProductCatalogDoesNotExistTest() {
+	public void OfferSpecailSaleForProductCatalogDoesNotExistTest() {
 		try {
 			Mockito.when(clientRequestHandler.sendRequestWithRespond(
-					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.GET_SALE_FOR_PRODUCT,
-							Serialization.serialize(barcode)).serialize()))
+					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.OFFER_SPECIAL_SALE_FOR_PRODUCT,
+							Serialization.serialize(sale)).serialize()))
 					.thenReturn(new CommandWrapper(ResultDescriptor.SM_CATALOG_PRODUCT_DOES_NOT_EXIST, Serialization.serialize(sale)).serialize());
 		} catch (IOException ¢) {
 			
 			fail();
 		}
 		try {
-			customer.getSaleForProduct(barcode);
+			customer.offerSpecailSaleForProduct(sale);
 		} catch ( CriticalError |  CustomerNotConnected | InvalidParameter e) {
 			fail();
 		} catch (ProductCatalogDoesNotExist e) {
@@ -128,18 +137,17 @@ public class GetSalesForProductTest {
 	}
 	
 	@Test
-	public void GetSaleForProductInvalidParameterTest() {
+	public void OfferSpecailSaleForProductInvalidParameterTest() {
 		try {
 			Mockito.when(clientRequestHandler.sendRequestWithRespond(
-					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.GET_SALE_FOR_PRODUCT,
-							Serialization.serialize(barcode)).serialize()))
+					new CommandWrapper(CustomerDefs.loginCommandSenderId, CommandDescriptor.OFFER_SPECIAL_SALE_FOR_PRODUCT,
+							Serialization.serialize(sale)).serialize()))
 					.thenReturn(new CommandWrapper(ResultDescriptor.SM_INVALID_PARAMETER, Serialization.serialize(sale)).serialize());
 		} catch (IOException ¢) {
-			
 			fail();
 		}
 		try {
-			customer.getSaleForProduct(barcode);
+			customer.offerSpecailSaleForProduct(sale);
 		} catch ( CriticalError |  CustomerNotConnected | ProductCatalogDoesNotExist  e) {
 			fail();
 		} catch (InvalidParameter e) {
