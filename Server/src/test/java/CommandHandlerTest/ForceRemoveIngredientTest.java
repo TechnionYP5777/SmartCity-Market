c355b534c90299867a3d9ce7d85d8d7210d57c3d
LiorBenAmi <benamilior89@gmail.com>
2017-07-01 16:49:24 +0300

    testing Server #522 - ForceRemoveIngredientTest

---
+++ Server/src/test/java/CommandHandlerTest/ForceRemoveIngredientTest.java
@@ -0,0 +1,150 @@
+package CommandHandlerTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.gson.Gson;
+
+import BasicCommonClasses.Ingredient;
+import ClientServerApi.CommandDescriptor;
+import ClientServerApi.CommandWrapper;
+import ClientServerApi.ResultDescriptor;
+import CommandHandler.CommandExecuter;
+import SQLDatabase.SQLDatabaseConnection;
+import SQLDatabase.SQLDatabaseException.ClientNotConnected;
+import SQLDatabase.SQLDatabaseException.CriticalError;
+import SQLDatabase.SQLDatabaseException.IngredientNotExist;
+import SQLDatabase.SQLDatabaseException.IngredientStillUsed;
+
+/**
+ * @author Lior Ben Ami
+ * @since 2017-07-01
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ForceRemoveIngredientTest {
+
+	@Mock
+	private SQLDatabaseConnection sqlDatabaseConnection;
+
+	@Before
+	public void setup() {
+		PropertyConfigurator.configure("../log4j.properties");
+	}
+	
+	private static final Ingredient ingredient = new Ingredient(0, "ingre");
+	private static final int senderID = 1;
+	
+	@Test
+	public void removeIngredientSuccessfulTest() {
+		String command = new CommandWrapper(senderID, CommandDescriptor.FORCE_REMOVE_INGREDIENT,
+				new Gson().toJson(ingredient, Ingredient.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.doNothing().when(sqlDatabaseConnection).removeIngredient(senderID, ingredient);
+		} catch (CriticalError | ClientNotConnected | IngredientNotExist | IngredientStillUsed e1) {
+			fail();
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_OK, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void removeIngredientCriticalErrorTest() {
+		String command = new CommandWrapper(senderID, CommandDescriptor.REMOVE_INGREDIENT,
+				new Gson().toJson(ingredient, Ingredient.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.doThrow(new CriticalError()) .when(sqlDatabaseConnection).removeIngredient(senderID, ingredient);
+		} catch (ClientNotConnected | IngredientNotExist | IngredientStillUsed e1) {
+			fail();
+		} catch (CriticalError e) {
+			/* success */
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_ERR, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void removeIngredientClientNotConnectedTest() {
+		String command = new CommandWrapper(senderID, CommandDescriptor.FORCE_REMOVE_INGREDIENT,
+				new Gson().toJson(ingredient, Ingredient.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.doThrow(new ClientNotConnected()) .when(sqlDatabaseConnection).removeIngredient(senderID, ingredient);
+		} catch (CriticalError | IngredientNotExist | IngredientStillUsed e1) {
+			fail();
+		} catch (ClientNotConnected e) {
+			/* success */
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_SENDER_IS_NOT_CONNECTED, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void removeIngredientIngredientNotExistTest() {
+		String command = new CommandWrapper(senderID, CommandDescriptor.FORCE_REMOVE_INGREDIENT,
+				new Gson().toJson(ingredient, Ingredient.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.doThrow(new IngredientNotExist()) .when(sqlDatabaseConnection).removeIngredient(senderID, ingredient);
+		} catch (CriticalError | ClientNotConnected | IngredientStillUsed e1) {
+			fail();
+		} catch (IngredientNotExist e) {
+			/* success */
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.PARAM_ID_IS_NOT_EXIST, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void removeIngredientIngredientStillUsedTest() {
+		String command = new CommandWrapper(senderID, CommandDescriptor.FORCE_REMOVE_INGREDIENT,
+				new Gson().toJson(ingredient, Ingredient.class)).serialize();
+		CommandExecuter commandExecuter = new CommandExecuter(command);
+		CommandWrapper out;
+		
+		try {
+			Mockito.doThrow(new IngredientStillUsed()) .when(sqlDatabaseConnection).removeIngredient(senderID, ingredient);
+		} catch (CriticalError | ClientNotConnected | IngredientNotExist e1) {
+			fail();
+		} catch (IngredientStillUsed e) {
+			/* success */
+		}
+		
+		out = commandExecuter.execute(sqlDatabaseConnection);
+		
+		assertEquals(ResultDescriptor.SM_INGREDIENT_STILL_IN_USE, out.getResultDescriptor());
+	}
+	
+	@Test
+	public void removeIngredientIllegalIngredientTest() {
+		assertEquals(ResultDescriptor.SM_ERR,
+				(new CommandExecuter(new CommandWrapper(1, CommandDescriptor.FORCE_REMOVE_INGREDIENT,
+						new Gson().toJson("", String.class)).serialize())).execute(sqlDatabaseConnection)
+								.getResultDescriptor());
+	}
+}
